a57e068ca2baa99af90e64df5e1feac6bc3301b4
godfather <ekansh.baweja@gmail.com>
2020-08-23 11:28:05 +0530

    Subscribe to the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,53 @@
 import React from 'react';
 // import './App.css';
-import { data } from '../data';
+import { data } from '../data'; //We want to load our movies data from store and not here
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
 
-function App() {
+class App extends React.Component {
+
+  componentDidMount() {
+    const { store } = this.props;
+    store.subscribe(() => {
+      console.log('UPDATED');
+      this.forceUpdate();
+    });
+    //make api call
+    //dispatch action
+    store.dispatch({
+      type: 'ADD_MOVIES',
+      movies: data
+    });
+
+    console.log('STATE', this.props.store.getState());
+  }
+
+  render() {
+    const movies = this.props.store.getState();
+    console.log("RENDER!");
   return (
     <div className="App">
       <Navbar />
       <div className="main" >
         <div className="tabs" >
-          <div className="tabs" > Movies </div>
-          <div className="tabs" > Favourites </div>
+          <div className="tab" > Movies </div>
+          <div className="tab" > Favourites </div>
         </div>
 
         <div className="list" >
-            {data.map(movie => (
-              <MovieCard movie = {movie} />
+            {movies.map((movie, index) => (
+              <MovieCard movie = {movie} key={`movies- ${index} `} />
             ))}
         </div>
 
       </div>
     </div>
   );
+  }
+  
 }
 
 export default App;
+
+// ComponentDidMount needed to be used here, so converted func comp to class comp
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 import App from './components/App';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log('store', store);
+
+// console.log('Before State', store.getState());
+// //Action object
+// //We can send actions to our store using dispatch func
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [{ name: 'Superman' }]
+// })
+// console.log('After State', store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store = {store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +28,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
